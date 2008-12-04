de05d76ec672a4f8cf18c394684c299b8e3ac462
childss <childss@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2008-12-04 17:01:25 +0000

    Added final snapshot from CERN CVS.
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/lcg-2/ncm-lcgmonjob@2 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

---
+++ ncm-lcgmonjob/src/main/pan/components/lcgmonjob/schema.pan
@@ -0,0 +1,19 @@
+# ${license-info}
+# ${developer-info}
+# ${author-info}
+# ${build-info}
+#
+
+declaration template components/lcgmonjob/schema;
+
+include { 'quattor/schema' };
+
+type ${project.artifactId}_component = {
+	include structure_component
+	'EDG_LOCATION' : string
+	'LCG_LOCATION' : string
+};
+
+bind '/software/components/${project.artifactId}' = ${project.artifactId}_component;
+
+
---
+++ ncm-lcgmonjob/src/main/perl/lcgmonjob.pm
@@ -0,0 +1,68 @@
+# ${license-info}
+# ${developer-info}
+# ${author-info}
+# ${build-info}
+#
+
+package NCM::Component::lcgmonjob;
+
+use strict;
+use NCM::Component;
+use vars qw(@ISA $EC);
+@ISA = qw(NCM::Component);
+$EC=LC::Exception::Context->new->will_store_all;
+use NCM::Check;
+
+use EDG::WP4::CCM::Element;
+
+use File::Copy;
+use File::Path;
+
+local(*DTA);
+
+
+##########################################################################
+sub Configure($$@) {
+##########################################################################
+    
+    my ($self, $config) = @_;
+
+    # Define paths for convenience. 
+    my $base = "/software/components/lcgmonjob";
+
+    # Get the EDG and LCG locations.
+    my $edgpath = "$base/EDG_LOCATION";
+    my $lcgpath = "$base/LCG_LOCATION";
+    my $edgloc = $config->getValue($edgpath);
+    my $lcgloc = $config->getValue($lcgpath);
+
+    # Define the source and destinations.
+    my $src = "$lcgloc/etc/init.d/lcg-mon-job-status";
+    my $dst = "/etc/rc.d/init.d/lcg-mon-job-status";
+
+    # Check that the source file exists.
+    if (! -f $src) {
+	$self->error("init.d script ($src) doesn't exist");
+	return 1;
+    }
+
+    # Remove the symbolic link if it exists. 
+    unlink $dst if (-l $dst);
+
+    # Create the new symbolic link.
+    my $rc = symlink $src, $dst;
+    if (! $rc) {
+	$self->error("symlink ($src, $dst) returned error");
+	return 1;
+    }
+
+    # Restart the daemon.  The restart method doesn't work so manually
+    # do a stop and start.
+    if (system($dst)) {
+	$self->error("$dst restart failed: ". $?);
+    }
+
+    return 1;
+}
+
+1;      # Required for PERL modules
