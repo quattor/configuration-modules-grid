4a4b0675a9ca29d2784147f5e54f51079a313f41
jouvin <jouvin@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2009-05-08 21:58:22 +0000

    Fix config rules for xroot
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/lcg-2/ncm-dpmlfc@882 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- ncm-dpmlfc/src/main/perl/dpmlfc.pm
+++ ncm-dpmlfc/src/main/perl/dpmlfc.pm
@@ -225,8 +225,8 @@ my $xroot_config_file = "/etc/sysconfig/dpm-xrd";
 my %xroot_config_rules = (
         "DPM_HOST" => "host:dpm;$line_format_envvar",
         "DPNS_HOST" => "host:dpns;$line_format_envvar",
-        "DPM_XROOTD_NOGSI" => "xroot->gsiEnabled:xroot",
-        "XRDCONFIG", => "xroot->config:xroot",
+        "DPM_XROOTD_NOGSI" => "xroot->gsiEnabled:xroot;$line_format_envvar",
+        "XRDCONFIG", => "xroot->config:xroot;$line_format_envvar",
        );
 
 my $trust_roles = "dpm,dpns,rfio,gsiftp";
