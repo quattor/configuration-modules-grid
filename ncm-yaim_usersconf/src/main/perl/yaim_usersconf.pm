54580ee37b7a3372adc0a1141715fb3ffff7b083
uschwick <uschwick@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-06-21 11:08:01 +0000

    add protection against double entries
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/lcg-2/ncm-yaim_usersconf@2807 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- ncm-yaim_usersconf/src/main/perl/yaim_usersconf.pm
+++ ncm-yaim_usersconf/src/main/perl/yaim_usersconf.pm
@@ -26,9 +26,9 @@ my $infobyvo   = $basedir . "/vo";
 ##########################################################################
 sub Configure($$@) {
 ##########################################################################
-
+    
     my ($self, $config) = @_;
-
+    
     my $usecache = 0;
     if ($config->elementExists("$basedir/usecache")){
         $usecache = $config->getElement("$basedir/usecache");
@@ -42,51 +42,58 @@ sub Configure($$@) {
     if ($config->elementExists($usersconf)){
         my %user_cache = my %gid_cache = ();
         if ($usecache){
-             my ($ref1,$ref2) = ReadCache($config->getValue($usersconf));
-             %user_cache = %$ref1 if $ref1;
-             %gid_cache  = %$ref2 if $ref2;
+	    my ($ref1,$ref2) = ReadCache($config->getValue($usersconf));
+	    %user_cache = %$ref1 if $ref1;
+	    %gid_cache  = %$ref2 if $ref2;
         }
         my $vo = $config->getElement("${infobyvo}");
         my $result = "";
+	my @seen = ();
         while ($vo->hasNextElement()) {
             my $voname = $vo->getNextElement()->getName();
             if ($config->elementExists("${infobyvo}/$voname/gridusers")){
-                 my $user_list = $config->getElement("${infobyvo}/$voname/gridusers");
-                 while ($user_list->hasNextElement()) {
-                     my $index = $user_list->getNextElement()->getName();
-                     my $path = "${infobyvo}/$voname/gridusers/$index";
-                     my $name = my $flag = undef;
-                     if ($config->elementExists("$path/name")){
-                         $name = $config->getValue("$path/name");
-                     }
-                     if ($config->elementExists("$path/flag")){
-                         $flag = $config->getValue("$path/flag");
-                     }
-                     my $uid = my $gid = undef;
-                     if (exists $user_cache{$name}){
-                         ($uid,$gid) = @{$user_cache{$name}};
-                         delete $user_cache{$name}; # delete used entries, to obtain minor speedup...
-                     }else{
-                         ($uid,$gid) = (getpwnam($name))[2,3];
-                     }
-                     if (not defined $uid or not defined $gid){
-                         $self->warn("Cannot get uid/gid belonging to username \"$name\", skipping...");
-                         next;
-                     }
-                     $gid_cache{$gid} ||= getgrgid($gid);
-                     my $gnam = $gid_cache{$gid};
-                     if (not defined $gnam){
-                         $self->warn("Cannot get group name belonging to group id \"$gid\", skipping...");
-                         next;
-                     }
-		     if ($flag && ($gid =~ /^\d+$/)){
-			 # special case: no secondary gid but flag defined so we reuse the primary
-			 $result .= join(":",$uid,$name,"${gid},${gid}","${gnam},${gnam}",$voname,$flag) . ":\n";
-		     } else {
-			 $flag ||= "";
-			 $result .= join(":",$uid,$name,$gid,$gnam,$voname,$flag) . ":\n";
-		     }
-                 }
+		my $user_list = $config->getElement("${infobyvo}/$voname/gridusers");
+		while ($user_list->hasNextElement()) {
+		    my $index = $user_list->getNextElement()->getName();
+		    my $path = "${infobyvo}/$voname/gridusers/$index";
+		    my $name = my $flag = undef;
+		    if ($config->elementExists("$path/name")){
+			$name = $config->getValue("$path/name");
+		    }
+		    if (grep {$name eq $_} @seen){
+			$self->warn("Ignoring duplicate entry \"$name\"");
+			next;
+		    } else {
+			push(@seen,$name);
+		    }
+		    if ($config->elementExists("$path/flag")){
+			$flag = $config->getValue("$path/flag");
+		    }
+		    my $uid = my $gid = undef;
+		    if (exists $user_cache{$name}){
+			($uid,$gid) = @{$user_cache{$name}};
+			delete $user_cache{$name}; # delete used entries, to obtain minor speedup...
+		    } else {
+			($uid,$gid) = (getpwnam($name))[2,3];
+		    }
+		    if (not defined $uid or not defined $gid){
+			$self->warn("Cannot get uid/gid belonging to username \"$name\", skipping...");
+			next;
+		    }
+		    $gid_cache{$gid} ||= getgrgid($gid);
+		    my $gnam = $gid_cache{$gid};
+		    if (not defined $gnam){
+			$self->warn("Cannot get group name belonging to group id \"$gid\", skipping...");
+			next;
+		    }
+		    if ($flag && ($gid =~ /^\d+$/)){
+			# special case: no secondary gid but flag defined so we reuse the primary
+			$result .= join(":",$uid,$name,"${gid},${gid}","${gnam},${gnam}",$voname,$flag) . ":\n";
+		    } else {
+			$flag ||= "";
+			$result .= join(":",$uid,$name,$gid,$gnam,$voname,$flag) . ":\n";
+		    }
+		}
             }
         }
         $result ||= "#\n# Empty file, do not remove!!\n#\n#         best regards, yaim_usersconf\n#\n";
@@ -99,10 +106,10 @@ sub Configure($$@) {
         print CFGFILE $result;
         close(CFGFILE);
         $self->info("Successfully written \"$cfgfile\"");
-    }else{
+    } else {
         $self->info("no location for users.conf file defined, skipping...");
     }
-
+    
     #
     # groups.conf
     #
@@ -112,23 +119,23 @@ sub Configure($$@) {
         while ($vo->hasNextElement() ) {
             my $voname = $vo->getNextElement()->getName();
             if ($config->elementExists("${infobyvo}/$voname/gridgroups")){
-                 my $group_list = $config->getElement("${infobyvo}/$voname/gridgroups");
-                 while ($group_list->hasNextElement()) {
-                     my $index = $group_list->getNextElement()->getName();
-                     my $path = "${infobyvo}/$voname/gridgroups/$index";
-                     my $role = my $flag = undef;
-                     if ($config->elementExists("$path/role")){
-                         $role = $config->getValue("$path/role");
-                     }else{
-                         $self->warn("Cannot find \"$path/role\", skipping...");
-                         next;
-                     }
-                     if ($config->elementExists("$path/flag")){
-                         $flag = $config->getValue("$path/flag");
-                     }
-                     $flag ||= "";
-                     $result .=  join(":",$role,"","",$flag) . ":\n";
-                 }
+		my $group_list = $config->getElement("${infobyvo}/$voname/gridgroups");
+		while ($group_list->hasNextElement()) {
+		    my $index = $group_list->getNextElement()->getName();
+		    my $path = "${infobyvo}/$voname/gridgroups/$index";
+		    my $role = my $flag = undef;
+		    if ($config->elementExists("$path/role")){
+			$role = $config->getValue("$path/role");
+		    } else {
+			$self->warn("Cannot find \"$path/role\", skipping...");
+			next;
+		    }
+		    if ($config->elementExists("$path/flag")){
+			$flag = $config->getValue("$path/flag");
+		    }
+		    $flag ||= "";
+		    $result .=  join(":",$role,"","",$flag) . ":\n";
+		}
             }
         }
         $result ||= "#\n# Empty file, do not remove!!\n#\n#         best regards, yaim_usersconf\n#\n";
@@ -141,7 +148,7 @@ sub Configure($$@) {
         print CFGFILE $result;
         close(CFGFILE);
         $self->info("Successfully written \"$cfgfile\"");
-    }else{
+    } else {
         $self->info("no location for groups.conf file defined, skipping...")
     }
     return;
@@ -160,7 +167,7 @@ sub ReadCache($){
         $gid_cache{$gid} ||= (split(",",$groupname))[0];
     }
     close(C);
-
+    
     return \%user_cache,\%gid_cache;
 }
 
