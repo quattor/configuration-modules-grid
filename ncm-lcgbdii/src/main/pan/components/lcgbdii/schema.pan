19e842a57e84ebd280e56a5cd7735e84a464467f
victor_mendoza <victor_mendoza@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2012-05-23 11:58:18 +0000

    ncm-lcgbdii: make most resources optional for EMI compatibility.
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/lcg-2/ncm-lcgbdii@3206 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- ncm-lcgbdii/src/main/pan/components/lcgbdii/schema.pan
+++ ncm-lcgbdii/src/main/pan/components/lcgbdii/schema.pan
@@ -13,71 +13,71 @@ include { 'pan/types' };
 # Function to check that some optional properties are present in context they
 # are requireed
 function lcgbdii_check_params = {
-  if ( ARGC != 1 ) {
-    error('lcgbdii_check_params must receive exactly one argument of type component_lcgbdii');
-  };
+    if ( ARGC != 1 ) {
+        error('lcgbdii_check_params must receive exactly one argument of type component_lcgbdii');
+    };
   
-  if ( (SELF['autoUpdate'] == 'yes') && !exists(SELF['updateUrl']) ) {
-    error('Property updateUrl required when autoUpdate=yes');
-  };
+    if ( (SELF['autoUpdate'] == 'yes') && !exists(SELF['updateUrl']) ) {
+        error('Property updateUrl required when autoUpdate=yes');
+    };
 
-  if ( (SELF['autoModify'] == 'yes') && !exists(SELF['updateLdif']) ) {
-    error('Property updateLdif required when autoModify=yes');
-  };
+    if ( (SELF['autoModify'] == 'yes') && !exists(SELF['updateLdif']) ) {
+        error('Property updateLdif required when autoModify=yes');
+    };
 
-  if ( !is_defined(SELF['port']) && (!is_defined(SELF['portRead']) || !is_defined(SELF['portsWrite'])) ) {
-      error('Either port or portRead/portsWrite must be specified');
-  };
+    if ( !is_defined(SELF['port']) && (!is_defined(SELF['portRead']) || !is_defined(SELF['portsWrite'])) ) {
+        error('Either port or portRead/portsWrite must be specified');
+    };
   
-  return(true);
+   true;
 };
 
 
 type ${project.artifactId}_component = {
-	include structure_component
+    include structure_component
 
-  'dir'           : string = '/opt/bdii/'
-  'varDir'        : string = '/opt/bdii/var'
-  'configFile'    : string = '/opt/bdii/bdii.conf'
-  'logFile'       ? string
-  'logLevel'      : string = "ERROR" with match(SELF,'^(ERROR|WARNING|INFO|DEBUG)$')
+    'dir'               ? string = '/opt/bdii/'
+    'varDir'            : string = '/opt/bdii/var'
+    'configFile'        : string = '/opt/bdii/bdii.conf'
+    'logFile'           ? string
+    'logLevel'          : string = "ERROR" with match(SELF,'^(ERROR|WARNING|INFO|DEBUG)$')
 
-  'schemaFile'    : string = '/opt/bdii/etc/schemas'
-  'schemas'       ? string[]
+    'schemaFile'        ? string = '/opt/bdii/etc/schemas'
+    'schemas'           ? string[]
 
-  'port'          ? type_port
-  'portRead'      ? type_port
-  'portsWrite'    ? type_port[]
-  'user'          : string = 'edguser'
-  'bind'          : string = 'mds-vo-name=local,o=grid'
-  'passwd'        : string
-  'searchFilter'  ? string
-  'searchTimeout' ? long(1..)
-  'readTimeout'   ? long(1..)
-  'breatheTime'   : long(1..) = 60
-  'archiveSize'   ? long
-  'autoUpdate'    : string = 'no' with match (SELF, '^(yes|no)$')
-  'autoModify'    : string = 'no' with match (SELF, '^(yes|no)$')
-  'isCache'       : string = 'no' with match (SELF, '^(yes|no)$')
-  'modifyDN'      : string = 'no' with match (SELF, '^(yes|no)$')
-  'RAMDisk'       ? string        with match (SELF, '^(yes|no)$')
-  'deleteDelay'   ? long
-  'fixGlue'       ? string        with match (SELF, '^(yes|no)$')
+    'port'              ? type_port
+    'portRead'          ? type_port
+    'portsWrite'        ? type_port[]
+    'user'              : string = 'edguser'
+    'bind'              ? string = 'mds-vo-name=local,o=grid'
+    'passwd'            ? string
+    'searchFilter'      ? string
+    'searchTimeout'     ? long(1..)
+    'readTimeout'       ? long(1..)
+    'breatheTime'       ? long(1..) = 60
+    'archiveSize'       ? long
+    'autoUpdate'        ? string = 'no' with match (SELF, '^(yes|no)$')
+    'autoModify'        ? string = 'no' with match (SELF, '^(yes|no)$')
+    'isCache'           ? string = 'no' with match (SELF, '^(yes|no)$')
+    'modifyDN'          ? string = 'no' with match (SELF, '^(yes|no)$')
+    'RAMDisk'           ? string with match (SELF, '^(yes|no)$')
+    'deleteDelay'       ? long
+    'fixGlue'           ? string with match (SELF, '^(yes|no)$')
 
-  'updateUrl'     ? type_absoluteURI
-  'updateLdif'    ? type_absoluteURI
-  'defaultLdif'   : string = '/opt/bdii/etc/default.ldif'
+    'updateUrl'         ? type_absoluteURI
+    'updateLdif'        ? type_absoluteURI
+    'defaultLdif'       ? string = '/opt/bdii/etc/default.ldif'
 
-  'slapd'         ? string
-  'slapadd'       ? string
-  'slapdConf'     : string = '/opt/bdii/etc/glue-slapd.conf'
-  'slapdDebugLevel' ? long(0..5)
+    'slapd'             ? string
+    'slapadd'           ? string
+    'slapdConf'         ? string = '/opt/bdii/etc/glue-slapd.conf'
+    'slapdDebugLevel'   ? long(0..5)
 
-  'urls'          ? type_absoluteURI{}
+    'urls'              ? type_absoluteURI{}
   
-  'ldifDir'       ? string
-  'pluginDir'     ? string
-  'providerDir'   ? string
+    'ldifDir'           ? string
+    'pluginDir'         ? string
+    'providerDir'       ? string
   
 } with lcgbdii_check_params(SELF);
 
--- ncm-lcgbdii/src/main/perl/lcgbdii.pm
+++ ncm-lcgbdii/src/main/perl/lcgbdii.pm
@@ -23,13 +23,15 @@ local(*DTA);
 
 # List of directories invalid for BDII working directories.
 # Value is useless
-my %forbiddenDirs = ('/' => '',
-                     '/opt' => '',
-                     '/tmp' => '',
-                     '/usr' => '',
-                     '/var' => '',
-                     '/var/log' => '',
-                    );
+my %forbiddenDirs = (
+    '/' => '',
+    '/etc' => '',
+    '/opt' => '',
+    '/tmp' => '',
+    '/usr' => '',
+    '/var' => '',
+    '/var/log' => '',
+);
 
 ##########################################################################
 sub Configure($$@) {
@@ -55,17 +57,17 @@ sub Configure($$@) {
     # Retrieve BDII working directory and create/update owner
     # Create the directory if necessary.
     my $varDir = $lcgbdii_config->{varDir};
-    unless ( $self->createAndChownDir($user,$varDir) ) {
-      return 1;
-    };
+    unless ( $self->createAndChownDir($user, $varDir) ) {
+        return 1;
+    }
     
     # Retrieve BDII logging directory and create/update owner
     # Create the directory if necessary.
     my $logFile = $lcgbdii_config->{logFile};
     if ( $logFile ) {
-      unless ( $self->createAndChownDir($user,dirname($logFile)) ) {
-        return 1;
-      };      
+        unless ( $self->createAndChownDir($user, dirname($logFile)) ) {
+            return 1;
+        }      
     }
     
 
@@ -75,15 +77,15 @@ sub Configure($$@) {
 
     # The configuration file location.
     unless ( defined($lcgbdii_config->{configFile}) ) {
-       $self->error("configuration file name not specified");
-       return 1;
+        $self->error("configuration file name not specified");
+        return 1;
     }
 
     # Create the directory if necessary.
     my $dir = dirname($lcgbdii_config->{configFile});
-    unless ( $self->createAndChownDir($user,$dir) ) {
-      return 1;
-    };
+    unless ( $self->createAndChownDir($user, $dir) ) {
+        return 1;
+    }
     
     # Fill template and get results.  Template substitution is simple
     # value replacement.  If a value doesn't exist, the line is not
@@ -99,7 +101,8 @@ sub Configure($$@) {
     # Now just create the new configuration file.  Be careful to save
     # a backup of the previous file if necessary. 
     # Already exists. Make backup and create new file. 
-    $result = LC::Check::file($lcgbdii_config->{configFile},
+    $result = LC::Check::file(
+        $lcgbdii_config->{configFile},
         backup => ".old",
         contents => $contents,
         mode => 0600,
@@ -107,14 +110,14 @@ sub Configure($$@) {
         group => (getpwnam($user))[3],
     );
     if ( $result > 0 ) {
-        $self->log($lcgbdii_config->{configFile}." updated");
+        $self->log($lcgbdii_config->{configFile} . " updated");
         $changes += $result;
         # make sure backup is secure
         LC::Check::mode(0600, $lcgbdii_config->{configFile} . ".old");
         LC::Check::owner((getpwnam($user))[2], $lcgbdii_config->{configFile} . ".old");
         LC::Check::group((getpwnam($user))[3], $lcgbdii_config->{configFile} . ".old");
     } elsif ( $result < 0 ) {
-        $self->error("Failed to update ".$lcgbdii_config->{configFile})
+        $self->error("Failed to update " . $lcgbdii_config->{configFile})
     }
 
 
@@ -122,44 +125,36 @@ sub Configure($$@) {
     # Build the BDII update configuration file #
     ############################################
     
-    # The update configuration file location.
-    unless ( defined($lcgbdii_config->{dir}) ) {
-        $self->error("BDII base directory not specified");
-        return 1;
-    }
-    my $bdiiConfDir = $lcgbdii_config->{dir}.'/etc';
-    unless ( $self->createAndChownDir($user,$bdiiConfDir) ) {
-      return 1;
-    };
-    my $fname = "$bdiiConfDir/bdii-update.conf";
+    if ( defined($lcgbdii_config->{urls}) && (length(%{$lcgbdii_config->{urls}}) > 0) ) {
+        # The update configuration file location.
+        my $bdiiConfDir = dirname($lcgbdii_config->{configFile});
+        my $fname = "$bdiiConfDir/bdii-update.conf";
 
-    # Create the contents.
-    $contents = "#\n# Created and maintained by ncm-lcgbdii. DO NOT EDIT.\n#\n";
-    
-    if ( defined($lcgbdii_config->{urls}) ) {
+        # Create the contents.
+        $contents = "#\n# Created and maintained by ncm-lcgbdii. DO NOT EDIT.\n#\n";
         foreach (sort keys %{$lcgbdii_config->{urls}}) {
             $contents .= $_ . " " . $lcgbdii_config->{urls}->{$_} . "\n";
         }
-    }
     
-    # Now just create the new configuration file.  Be careful to save
-    # a backup of the previous file if necessary. 
-    $result = LC::Check::file($fname,
-        backup => ".old",
-        contents => $contents,
-        mode => 0600,
-        owner => (getpwnam($user))[2],
-        group => (getpwnam($user))[3],
-    );
-    if ( $result > 0 ) {
-        $self->log($fname." updated");
-        $changes += $result;
-        # make sure backup is secure
-        LC::Check::mode(0600, $fname . ".old");
-        LC::Check::owner((getpwnam($user))[2], $fname . ".old");
-        LC::Check::group((getpwnam($user))[3], $fname . ".old");
-    } elsif ( $result < 0 ) {
-        $self->error("Failed to update ".$fname)
+        # Now just create the new configuration file.  Be careful to save
+        # a backup of the previous file if necessary. 
+        $result = LC::Check::file($fname,
+            backup => ".old",
+            contents => $contents,
+            mode => 0600,
+            owner => (getpwnam($user))[2],
+            group => (getpwnam($user))[3],
+        );
+        if ( $result > 0 ) {
+            $self->log($fname . " updated");
+            $changes += $result;
+            # make sure backup is secure
+            LC::Check::mode(0600, $fname . ".old");
+            LC::Check::owner((getpwnam($user))[2], $fname . ".old");
+            LC::Check::group((getpwnam($user))[3], $fname . ".old");
+        } elsif ( $result < 0 ) {
+            $self->error("Failed to update " . $fname)
+        }
     }
 
 
@@ -168,34 +163,35 @@ sub Configure($$@) {
     ######################################
     
     if ( defined($lcgbdii_config->{schemas}) && (length(@{$lcgbdii_config->{schemas}}) > 0) ) {
-      # The schema file location.
-      unless ( defined($lcgbdii_config->{schemaFile}) ) {
-          $self->error("BDII schema file name not specified");
-          return 1;
-      }
+        # The schema file location.
+        unless ( defined($lcgbdii_config->{schemaFile}) ) {
+            $self->error("BDII schema file name not specified");
+            return 1;
+        }
   
-      # Create the directory if necessary.
-      $dir = dirname($lcgbdii_config->{schemaFile});
-      $self->createDir($dir);
+        # Create the directory if necessary.
+        $dir = dirname($lcgbdii_config->{schemaFile});
+        $self->createDir($dir);
   
-      # Create the contents.  Just a list of the schema files.
-      $contents = '';
-      foreach (@{$lcgbdii_config->{schemas}}) {
-          $contents .= $_ . "\n";
-      }
+        # Create the contents.  Just a list of the schema files.
+        $contents = '';
+        foreach (@{$lcgbdii_config->{schemas}}) {
+            $contents .= $_ . "\n";
+        }
   
-      # Now just create the new configuration file.  Be careful to save
-      # a backup of the previous file if necessary. 
-      $result = LC::Check::file($lcgbdii_config->{schemaFile},
-                                   backup => ".old",
-                                   contents => $contents,
-                                  );
-      if ( $result > 0 ) {
-          $self->log($lcgbdii_config->{schemaFile}." updated");
-          $changes += $result;
-      } elsif ( $result < 0 ) {
-          $self->error("Failed to update ".$lcgbdii_config->{schemaFile})
-      }
+        # Now just create the new configuration file.  Be careful to save
+        # a backup of the previous file if necessary. 
+        $result = LC::Check::file(
+            $lcgbdii_config->{schemaFile},
+            backup => ".old",
+            contents => $contents,
+        );
+        if ( $result > 0 ) {
+            $self->log($lcgbdii_config->{schemaFile} . " updated");
+            $changes += $result;
+        } elsif ( $result < 0 ) {
+            $self->error("Failed to update " . $lcgbdii_config->{schemaFile})
+        }
     }
 
 
@@ -204,12 +200,12 @@ sub Configure($$@) {
     ################################    
 
     if ( $changes ) {
-      if (system("/sbin/service bdii stop")) {
-          $self->warn("init.d lcg-bdii stop failed: ". $?);
-      }
-      if (system("/sbin/service bdii start")) {
-          $self->error("init.d lcg-bdii start failed: ". $?);
-      }
+        if (system("/sbin/service bdii stop")) {
+            $self->warn("init.d lcg-bdii stop failed: " . $?);
+        }
+        if (system("/sbin/service bdii start")) {
+            $self->error("init.d lcg-bdii start failed: " . $?);
+        }
     }
     
 
@@ -229,35 +225,35 @@ sub Configure($$@) {
 #
 sub fill_template {
 
-    my ($self,$config, $base, $template) = @_;
+    my ($self, $config, $base, $template) = @_;
 
     my $translation = "";
 
     if (-e "$template") {
-    open TMP, "<$template";
-    while (<TMP>) {
+        open TMP, "<$template";
+        while (<TMP>) {
             my $err = 0;
 
             # Special form for date.
             s/<%!date!%>/localtime()/eg;
 
             # Need quoted result (escape embedded quotes).
-            s!<%"\s*(/[\w/]+)\s*(?:\|\s*(.+?))?\s*"%>!quote($self->fill($config,$1,$2,\$err))!eg;
-            s!<%"\s*([\w]+[\w/]*)(?:\|\s*(.+?))?\s*"%>!quote($self->fill($config,"$base/$1",$2,\$err))!eg;
+            s!<%"\s*(/[\w/]+)\s*(?:\|\s*(.+?))?\s*"%>!quote($self->fill($config, $1, $2, \$err))!eg;
+            s!<%"\s*([\w]+[\w/]*)(?:\|\s*(.+?))?\s*"%>!quote($self->fill($config, "$base/$1", $2, \$err))!eg;
 
             # Normal result OK. 
-            s!<%\s*(/[\w/]+)\s*(?:\|\s*(.+?))?%>!$self->fill($config,$1,$2,\$err)!eg;
-            s!<%\s*([\w]+[\w/]*)\s*(?:\|\s*(.+?))?%>!$self->fill($config,"$base/$1",$2,\$err)!eg;
+            s!<%\s*(/[\w/]+)\s*(?:\|\s*(.+?))?%>!$self->fill($config, $1, $2, \$err)!eg;
+            s!<%\s*([\w]+[\w/]*)\s*(?:\|\s*(.+?))?%>!$self->fill($config, "$base/$1", $2, \$err)!eg;
 
             # Add the output line unless an error was signaled.  An
             # error occurs when an element doesn't exist.  In this
             # case it is assumed that the value is optional and the
             # line is omitted.  
             $translation .= $_ unless $err;
-    }
-    close TMP;
+        }
+        close TMP;
     } else {
-    $translation = undef;
+        $translation = undef;
     }
 
     return $translation;
@@ -269,15 +265,15 @@ sub fill_template {
 # Escape quotes in a string value.
 # Return list as a quoted space-separated string.
 sub fill {
-    my ($self,$config,$path,$default,$errorRef) = @_;
+    my ($self, $config, $path, $default, $errorRef) = @_;
 
     my $value = "";
 
     if ($config->elementExists($path)) {
         if ( $config->getElement($path)->isType($config->getElement($path)->LIST) ) {
-            $self->debug(2,"$path is a list. Converting to a space-separated string");
+            $self->debug(2, "$path is a list. Converting to a space-separated string");
             $value = '"' . join(' ', @{$config->getElement($path)->getTree()}) . '"';
-            $self->debug(2,"$path value converted to string $value");
+            $self->debug(2, "$path value converted to string $value");
         } else {
             $value = $config->getValue($path);
         }
@@ -296,7 +292,7 @@ sub quote {
     my ($value) = @_;
 
     $value =~ s/"/\\"/g;  # escape any embedded quotes
-    $value = '"'.$value.'"';
+    $value = '"' . $value . '"';
     return $value;
 }
 
@@ -309,27 +305,27 @@ sub createAndChownDir {
     my ($self, $user, $dir) = @_;
     
     if ( defined($forbiddenDirs{$dir}) ) {
-      $self->error("$dir is a system directory and cannot be used as a BDII-specific directory."); 
-      return 0;      
+        $self->error("$dir is a system directory and cannot be used as a BDII-specific directory."); 
+        return 0;      
     }
 
     unless ( $self->createDir($dir) ) {
-      return 0;
-    };
+        return 0;
+    }
   
-    my ($uid,$gid) = (getpwnam($user))[2,3];
+    my ($uid, $gid) = (getpwnam($user))[2, 3];
     unless ( defined($uid) ) {
-      $self->error("Failed to retrieved uid for user $user");
-      return 0;
+        $self->error("Failed to retrieved uid for user $user");
+        return 0;
     }
     unless ( defined($gid) ) {
-      $self->error("Failed to retrieved gid for user $user");
-      return 0;
+        $self->error("Failed to retrieved gid for user $user");
+        return 0;
     }
     
-    unless ( $self->chownDirAndChildren($uid,$gid,$dir) ) {
-      return 0;
-    };
+    unless ( $self->chownDirAndChildren($uid, $gid, $dir) ) {
+        return 0;
+    }
     
     return 1;
 }
@@ -339,15 +335,15 @@ sub createAndChownDir {
 sub createDir {
     my ($self, $dir) = @_;
 
-    mkpath($dir,0,0755);
+    mkpath($dir, 0, 0755);
     # If a file with the same name already existed, throw an error.
     if ( !-d $dir ) {
-      if ( -e $dir ) {
-        $self->error("$dir exists but is not a directory");
-      } else {
-        $self->error("Failed to create directory $dir");
-      }
-      return 0;
+        if ( -e $dir ) {
+            $self->error("$dir exists but is not a directory");
+        } else {
+            $self->error("Failed to create directory $dir");
+        }
+        return 0;
     }
     
     return 1;
@@ -357,39 +353,39 @@ sub createDir {
 # This method also checks that the directory/file is not one of the standard directories.
 # Returns 1 in case of success, else 0.
 sub chownDirAndChildren {
-  my ($self, $uid, $gid, $file) = @_;
-  if ( @_ != 4 ) {
-    $self->error('chownDirAndChildren method requires 3 argments');
-    return 0;
-  }
-  unless ( defined($file) && (length($file) > 0) ) {
-    $self->error('directory/file name not specified');
-    return 0;
-  }
- 
-  if ( defined($forbiddenDirs{$file}) ) {
-    $self->error("$file is a system directory: its permission cannot be changed to a BDII-specific one."); 
-    return 0;      
-  }
-
-  $self->debug(1,"Updating $file owner to uid=$uid, gid=$gid");
-  chown($uid,$gid,$file);
-
-  # If $file is a directory, process its contents recursively (files and directories only)
-  if ( -d $file ) {
-    my @children = glob("$file/*");
-    for my $child (@children) {
-      if ( (-f $child || -d $child) && !-l $child && ($child ne $file) ) {
-        unless ($self->chownDirAndChildren ($uid,$gid,$child) ) {
-          return 0;
-        }
-      } else {
-        $self->debug(2,"$child is neither a directory nor a file. Ignoring...");
-      }
+    my ($self, $uid, $gid, $file) = @_;
+    if ( @_ != 4 ) {
+        $self->error('chownDirAndChildren method requires 3 argments');
+        return 0;
     }
-  }
+    unless ( defined($file) && (length($file) > 0) ) {
+        $self->error('directory/file name not specified');
+        return 0;
+    }
+ 
+    if ( defined($forbiddenDirs{$file}) ) {
+        $self->error("$file is a system directory: its permission cannot be changed to a BDII-specific one."); 
+        return 0;      
+    }
+
+    $self->debug(1, "Updating $file owner to uid=$uid, gid=$gid");
+    chown($uid, $gid, $file);
+
+    # If $file is a directory, process its contents recursively (files and directories only)
+    if ( -d $file ) {
+        my @children = glob("$file/*");
+        for my $child (@children) {
+            if ( (-f $child || -d $child) && !-l $child && ($child ne $file) ) {
+                unless ($self->chownDirAndChildren ($uid, $gid, $child) ) {
+                    return 0;
+                }
+            } else {
+                $self->debug(2, "$child is neither a directory nor a file. Ignoring...");
+            }
+        }
+    }
   
-  return 1;
+    return 1;
 }
 
-1;      # Required for PERL modules
+1; # Required for PERL modules
