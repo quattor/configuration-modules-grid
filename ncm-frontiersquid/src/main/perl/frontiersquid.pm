quattor/configuration-modules-grid

commit d841aee441cd20a507beaa57aa434b9ed52fe1b5

2010-02-25  Flavia Donno
	* Release: 1.3.0
	- Version working with frontier-squid rpm release 2.7.STABLE7-8



git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/lcg-2/ncm-frontiersquid@1739 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

ncm-frontiersquid/src/main/perl/frontiersquid.pm
@@ -26,56 +26,47 @@ use Encode qw(encode_utf8);
 local(*DTA);
 
 
-use EDG::WP4::CCM::Element;
-
 ##########################################################################
-sub Configure($$) {
+sub Configure($$@) {
 ##########################################################################
   my ($self,$config)=@_;
   my $base     = "/software/components/frontiersquid/";
-  my $rpm_home = "/home/dbfrontier";
-  my $response_file = "/tmp/response_file.ncm-frontiersquid";
+  my $rpm_home = "/data/squid";
+  my $response_file = "/data/squid/squidconf";
 
   my $frontier_config = $config->getElement($base)->getTree();
 
-  my $home      = $frontier_config->{home};
-  my $username  = $frontier_config->{username};
-  my $group     = $frontier_config->{group};
-  my $networks  = $frontier_config->{networks};
-  my $servers   = $frontier_config->{servers};
+  # Check that networks is really defined.
+  my $networks = $frontier_config->{networks};
+  if (!defined($networks)) {
+     $self->error("entry with undefined networks");
+     return 0;
+  }
+
+  # Pull out the other values
+  my $username = $frontier_config->{username};
+  my $group = $frontier_config->{group};
   my $cache_mem = $frontier_config->{cache_mem};
   my $cache_dir = $frontier_config->{cache_dir};
 
-  my $squid_file = $home."/etc/squid.conf";
-
-  my $contents  = $home."\n";
-  $contents  .= $username . "\n";
-  $contents  .= $group . "\n";
-  for my $net (@{$networks}) {
-	$contents .= EDG::WP4::CCM::Element::unescape($net).",";
-  }
-  $contents = substr($contents, 0, - 1); 
-  $contents  .= "\n";
-  for my $serv (@{$servers}) {
-        $contents .= EDG::WP4::CCM::Element::unescape($serv).",";
-  }
-  $contents = substr($contents, 0, - 1); 
-  $contents  .= "\n";
-  $contents  .= $cache_mem . "\n";
-  $contents  .= $cache_dir . "\n"; 
+  my $contents  = "export FRONTIER_USER=".$username."\n";
+  $contents  .= "export FRONTIER_GROUP=".$group . "\n";
+  $contents  .= "export FRONTIER_NET_LOCAL='".$networks."'\n"; 
+  $contents  .= "export FRONTIER_CACHE_MEM=".$cache_mem . "\n";
+  $contents  .= "export FRONTIER_CACHE_DIR_SIZE=".$cache_dir . "\n"; 
 
   my $changes = LC::Check::file("$response_file",
                                 contents => encode_utf8($contents),
                                 mode => 0644,
-                               );
+                                );
   if ( $changes < 0 ) {
 	$self->warn("Error creating $response_file");
+        return 0;
   }
 
-  system($rpm_home."/etc/post_install <".$response_file);
+  system("export SCFILE=".$response_file.";".$rpm_home."/etc/post_install");
 
-  return; # return code is not checked.
-
+  return 1; # return code is not checked.
 }
 
 1; # Perl module requirement.

ncm-frontiersquid/pom.xml
@@ -9,7 +9,7 @@
   <artifactId>frontiersquid</artifactId>
 
   <packaging>pom</packaging>
-  <version>1.0.2-SNAPSHOT</version>
+  <version>1.3.0-SNAPSHOT</version>
   <name>Used to manage FroNTier squid server</name>
 
   <repositories>

ncm-frontiersquid/src/main/pan/components/frontiersquid/schema.pan
@@ -10,13 +10,11 @@ include { 'quattor/schema' };
 
 type ${project.artifactId}_component_type = {
     include structure_component
-	'home'      : string = '/home/dbfrontier'
-	'username'  : string = 'dbfrontier'
-	'group'     : string = 'dbfrontier'
-	'networks'   : string[]
-	'servers'   : string[]
-	'cache_mem' : long = 2000
-	'cache_dir' : long = 20000
+	'username'  : string = 'squid'
+	'group'     : string = 'squid'
+	'networks'   : string
+	'cache_mem' : long = 128
+	'cache_dir' : long = 40000
 };
 
 bind "/software/components/frontiersquid" = ${project.artifactId}_component_type;

ncm-frontiersquid/src/main/perl/frontiersquid.pod
@@ -6,7 +6,14 @@
 
 =head1 NAME
 
-NCM::frontiersquid - Components used to manage FroNTier squid server
+NCM::frontiersquid - Component used to manage FroNTier squid server
+
+=head1 DESCRIPTION
+
+Object to configure and start the squid server for FroNTier.
+It creates a file where environmental variables needed by the squid server
+post_install script are defined. The post_install script is then
+explicitely called to configure and start the squid server.
 
 =head1 SYNOPSIS
 
@@ -14,7 +21,9 @@ NCM::frontiersquid - Components used to manage FroNTier squid server
 
 =item Configure()
 
-TBD
+It reads the variables set by the system administrator and creates the
+squidconf file. Such a file is then sourced before executing the
+squid server post_install script.
 
 =item Unconfigure()
 
@@ -24,14 +33,44 @@ Not implemented.
 
 =head1 RESOURCES
 
-=over
+=head2 /software/components/frontiersquid/active : boolean
 
-=item /software/components/frontiersquid/active : boolean
+Activates/deactivates the component.
 
-activates/deactivates the component.
+=head2 /software/components/frontiersquid/username
 
-=back
+Defines the username of the account the squid server runs under. 
+The default is squid.
 
+=head2 /software/components/frontiersquid/group
+
+Defines the group of the account the squid server runs under.
+The default is squid.
+
+=head2 /software/components/frontiersquid/networks
+
+This argument is mandatory. It lists the set of networks enabled to
+send requests to the squid server. The format of such argument must
+be "network/submask network/submask". Please see the EXAMPLE section.
+
+=head2 /software/components/frontiersquid/cache_mem
+
+Defines the amount of physical memory dedicated to squid cache.
+It is expressed in MB. The default is 128.
+
+=head2 /software/components/frontiersquid/cache_dir
+
+Defines the amount of space on disk dedicated to the squid cache.
+It is expressed in MB. The default is 40000.
+
+=head1 EXAMPLES
+
+=begin text
+include { 'components/frontiersquid/config' };
+"/software/components/frontiersquid/username" = "atsquid";
+"/software/components/frontiersquid/group" = "zp";
+"/software/components/frontiersquid/networks" = "128.141.0.0/255.255.0.0 137.138.0.0/255.255.0.0 172.17.0.0/255.255.0.0 172.18.0.0/255.255.0.0";
+=end text
 
 =head1 DEPENDENCIES
 
@@ -43,20 +82,17 @@ none.
 
 none.
 
-
 =head1 FILES
 
-This component doesn't touch any file.
-
+This component creates the file /data/squid/squidconf.
 
 =head1 BUGS
 
 None.
 
-
 =head1 AUTHOR
 
-German Cancio <German.Cancio@cern.ch>
+Flavia Donno <Flavia.Donno@cern.ch>
 
 =head1 SEE ALSO
 

commit 8c28cc967b1dd95fed546c9443e1aa72ddfa205c

Fix typo introduced in r1739.
git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/lcg-2/ncm-frontiersquid@3383 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

ncm-frontiersquid/src/main/perl/frontiersquid.pm
@@ -74,7 +74,7 @@ sub Configure($$@) {
         return 0;
   }
 
-  system("export SCFILE=".$response_file.";".$rpmhome.$postinstall");
+  system("export SCFILE=".$response_file.";".$rpmhome.$postinstall);
 
   return 1; # return code is not checked.
 }